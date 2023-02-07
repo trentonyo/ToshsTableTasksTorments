743c5c98f3b3888ca327c61839e6836964d034e0
Joshua Williams <joshuaw@qualtrics.com>
2023-02-06 20:48:03 -0700

    ddl v1

---
+++ ddl.sql
@@ -0,0 +1,81 @@
+CREATE TABLE QuestGivers (
+    questGiverId INT NOT NULL,
+    questGiverName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (questGiverId)
+);
+
+CREATE TABLE MonsterTypes (
+    monsterTypeId INT NOT NULL,
+    monsterTypeName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (monsterTypeId)
+);
+
+CREATE TABLE Monsters (
+    monsterId INT NOT NULL AUTO_INCREMENT,
+    monsterName VARCHAR(255) NOT NULL,
+    monsterDesc VARCHAR(255) NOT NULL,
+    monsterTypeId INT NOT NULL,
+    healthPool INT NULL,
+    attack INT NULL,
+    defense INT NULL,
+    speed INT NULL,
+    PRIMARY KEY (monsterId),
+    FOREIGN KEY (monsterTypeId) REFERENCES MonsterTypes(monsterTypeId)
+);
+
+CREATE TABLE Quests (
+    questId INT NOT NULL AUTO_INCREMENT,
+    questDesc VARCHAR(255) NOT NULL,
+    available BOOLEAN NOT NULL,
+    questGiverId INT NOT NULL,
+    suggestedLevel INT NOT NULL,
+    monsterQty INT NOT NULL,
+    monsterId INT NOT NULL,
+    rewardXp INT NOT NULL,
+    rewardGold INT NOT NULL,
+    PRIMARY KEY (questId),
+    FOREIGN KEY (questGiverId) REFERENCES QuestGivers(questGiverId),
+    FOREIGN KEY (monsterId) REFERENCES Monsters(monsterId)
+);
+
+CREATE TABLE Abilities (
+    abilityId INT NOT NULL AUTO_INCREMENT,
+    abilityName VARCHAR(255) NOT NULL,
+    abilityDesc VARCHAR(255) NOT NULL,
+    PRIMARY KEY (abilityId)
+);
+
+CREATE TABLE Monsters_Abilities (
+    monsterId INT NOT NULL,
+    abilityId INT NOT NULL,
+    abilityCooldown INT NULL,
+    PRIMARY KEY (monsterId, abilityId),
+    FOREIGN KEY (monsterId) REFERENCES Monsters(monsterId),
+    FOREIGN KEY (abilityId) REFERENCES Abilities(abilityId)
+);
+
+CREATE TABLE LootItemTypes (
+    lootItemTypeId INT NOT NULL,
+    lootItemTypeName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (lootItemTypeId)
+);
+
+CREATE TABLE LootItems (
+    lootId INT NOT NULL AUTO_INCREMENT,
+    lootName VARCHAR(255) NOT NULL,
+    lootDesc VARCHAR(255) NOT NULL,
+    lootItemTypeId INT NOT NULL,
+    lootValue INT NOT NULL,
+    PRIMARY KEY (lootId),
+    FOREIGN KEY (lootItemTypeId) REFERENCES LootItemTypes(lootItemTypeId)
+);
+
+CREATE TABLE Monsters_LootItems (
+    monsterId INT NOT NULL,
+    lootId INT NOT NULL,
+    dropQuantity INT NOT NULL,
+    dropChance DECIMAL NOT NULL,
+    PRIMARY KEY (monsterId, lootId),
+    FOREIGN KEY (monsterId) REFERENCES Monsters(monsterId),
+    FOREIGN KEY (lootId) REFERENCES LootItems(lootId)
+);
