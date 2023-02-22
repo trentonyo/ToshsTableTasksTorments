6113510ad078b7f0bc019102ca73283ec14fed56
TrentonYo <trentonyo@gmail.com>
2023-02-21 22:33:03 -0800

    Button now adds form in the new monster page

--- src/ux.js
+++ src/ux.js
@@ -2,6 +2,12 @@ console.log("Loaded ux.js")
 
 let input_fields = document.getElementsByTagName("input")
 
+let column_ability = document.getElementById("abilities_column")
+let column_loot = document.getElementById("lootItems_column")
+
+let add_ability = document.getElementById("addAbilityForm")
+let add_loot = document.getElementById("addLootItemForm")
+
 for (let i = 0; i < input_fields.length; i++)
 {
     let plc = input_fields[i].placeholder
@@ -15,3 +21,13 @@ for (let i = 0; i < input_fields.length; i++)
     }
 }
 
+add_ability.addEventListener("click", function (e)
+{
+    addAbilitiesForm()
+    updateGradientElements()
+})
+add_loot.addEventListener("click", function (e)
+{
+    addLootItemsForm()
+    updateGradientElements()
+})
