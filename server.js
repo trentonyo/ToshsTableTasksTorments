779f18e218880e11ca78e777636e9999d3aea5e9
TrentonYo <trentonyo@gmail.com>
2023-02-24 13:07:35 -0800

    Rounded out infobands

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const ENTITIES = {
     "LootItems" : {id: "lootId", en_singular: "Loot Item", en_plural: "Loot Item"},
     "LootItemTypes" : {id: "lootItemTypeId", en_singular: "Loot Item Type", en_plural: "Loot Item Types"},
     "Abilities" : {id: "abilityId", en_singular: "Ability", en_plural: "Abilities"},
-    "MonstersAbilities" : {id: "monsterAbilityId", en_singular: "Monster Ability", en_plural: "Monster Abilities"}, //TODO implement proper ID
-    "MonstersLootItems" : {id: "monsterLootItemId", en_singular: "Monster Loot Item", en_plural: "Monster Loot Items"}, //TODO implement proper ID
+    "MonstersAbilities" : {id: ["monsterId", "abilityId"], en_singular: "Monster Ability", en_plural: "Monster Abilities"},
+    "MonstersLootItems" : {id: ["monsterId", "lootId"], en_singular: "Monster Loot Item", en_plural: "Monster Loot Items"},
 }
 
 // Server
