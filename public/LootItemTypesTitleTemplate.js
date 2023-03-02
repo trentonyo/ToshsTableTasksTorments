256b8cfdd90c6f2193d50f5aa91710417452cff6
Trenton Young <trentonyo@gmail.com>
2023-03-02 14:53:06 -0800

    Added title to LootItemTypes

--- public/LootItemTypesTitleTemplate.js
+++ public/LootItemTypesTitleTemplate.js
@@ -1,6 +1,14 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['LootItemTypesTitle'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
+    var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return container.escapeExpression(((helper = (helper = lookupProperty(helpers,"lootItemTypeName") || (depth0 != null ? lookupProperty(depth0,"lootItemTypeName") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"lootItemTypeName","hash":{},"data":data,"loc":{"start":{"line":1,"column":0},"end":{"line":1,"column":20}}}) : helper)))
+    + "<span class=\"page_subtitle\">- Loot Item Type</span>";
 },"useData":true});
 })();
