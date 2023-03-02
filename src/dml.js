e025138192f2f16e229940f23a44c89be02b99eb
Trenton Young <trentonyo@gmail.com>
2023-03-02 14:47:01 -0800

    Added quotes to statement strings

--- src/dml.js
+++ src/dml.js
@@ -1,18 +1,18 @@
 let STATEMENTS = {
     INSERT_Quests:              function (questName, questDesc, available, questGiverId, suggestedLevel, monsterQty, monsterId, rewardXp, rewardGold) { /* TODO A foreign key constraint fails */
-                                    return `INSERT INTO Quests (questName, questDesc, available, questGiverId, suggestedLevel, monsterQty, monsterId, rewardXp, rewardGold) VALUES (${questName}, ${questDesc}, ${available}, ${questGiverId}, ${suggestedLevel}, ${monsterQty}, ${monsterId}, ${rewardXp}, ${rewardGold});` },
+                                    return `INSERT INTO Quests (questName, questDesc, available, questGiverId, suggestedLevel, monsterQty, monsterId, rewardXp, rewardGold) VALUES ('${questName}', '${questDesc}', ${available}, ${questGiverId}, ${suggestedLevel}, ${monsterQty}, ${monsterId}, ${rewardXp}, ${rewardGold});` },
     INSERT_QuestGivers:         function (questGiverName) {
-                                    return `INSERT INTO QuestGivers (questGiverName) VALUES (${questGiverName});` },
+                                    return `INSERT INTO QuestGivers (questGiverName) VALUES ('${questGiverName}');` },
     INSERT_Monsters:            function (monsterName, monsterDesc, monsterTypeId, healthPool, attack, defense, speed) {
-                                    return `INSERT INTO Monsters (monsterName, monsterDesc, monsterTypeId, healthPool, attack, defense, speed) VALUES (${monsterName}, ${monsterDesc}, ${monsterTypeId}, ${healthPool}, ${attack}, ${defense}, ${speed});` },
+                                    return `INSERT INTO Monsters (monsterName, monsterDesc, monsterTypeId, healthPool, attack, defense, speed) VALUES ('${monsterName}', '${monsterDesc}', ${monsterTypeId}, ${healthPool}, ${attack}, ${defense}, ${speed});` },
     INSERT_MonsterTypes:        function (monsterTypeName) {
-                                    return `INSERT INTO MonsterTypes (monsterTypeName) VALUES (${monsterTypeName});` },
+                                    return `INSERT INTO MonsterTypes (monsterTypeName) VALUES ('${monsterTypeName}');` },
     INSERT_Abilities:           function (abilityName, abilityDesc) {
-                                    return `INSERT INTO Abilities (abilityName, abilityDesc) VALUES (${abilityName}, ${abilityDesc});` },
+                                    return `INSERT INTO Abilities (abilityName, abilityDesc) VALUES ('${abilityName}', '${abilityDesc}');` },
     INSERT_LootItemTypes:       function (lootItemTypeName, equipable) {
-                                    return `INSERT INTO LootItemTypes (lootItemTypeName, equipable) VALUES (${lootItemTypeName}, ${equipable});` },
+                                    return `INSERT INTO LootItemTypes (lootItemTypeName, equipable) VALUES ('${lootItemTypeName}', ${equipable});` },
     INSERT_LootItems:           function (lootName, lootDesc, lootItemTypeId, lootValue) {
-                                    return `INSERT INTO LootItems (lootName, lootDesc, lootItemTypeId, lootValue) VALUES (${lootName}, ${lootDesc}, ${lootItemTypeId}, ${lootValue});` },
+                                    return `INSERT INTO LootItems (lootName, lootDesc, lootItemTypeId, lootValue) VALUES ('${lootName}', '${lootDesc}', ${lootItemTypeId}, ${lootValue});` },
     INSERT_Monsters_Abilities:  function (monsterId, abilityId, abilityCooldown) {
                                     return `INSERT INTO Monsters_Abilities (monsterId, abilityId, abilityCooldown) VALUES (${monsterId}, ${abilityId}, ${abilityCooldown});` },
     INSERT_Monsters_LootItems:  function (monsterId, lootId, dropQuantity, dropChance) {
@@ -41,9 +41,9 @@ let STATEMENTS = {
     SELECT_Monsters_LootItemsByID : function (monsterId, lootId) { return `SELECT * FROM Monsters_LootItems WHERE (monsterId=${monsterId} AND lootId=${lootId});` },
 
     UPDATE_Quests :     function (questId, questName, questDesc, available, questGiverId, suggestedLevel, monsterQty, monsterId, rewardXp, rewardGold) {
-                            return `UPDATE Quests SET questName=${questName}, questDesc=${questDesc}, available=${available}, questGiverId=${questGiverId}, suggestedLevel=${suggestedLevel}, monsterQty=${monsterQty}, rewardXp=${rewardXp}, rewardGold=${rewardGold}, monsterId=${monsterId} WHERE questId=${questId};` },
+                            return `UPDATE Quests SET questName='${questName}', questDesc='${questDesc}', available=${available}, questGiverId=${questGiverId}, suggestedLevel=${suggestedLevel}, monsterQty=${monsterQty}, rewardXp=${rewardXp}, rewardGold=${rewardGold}, monsterId=${monsterId} WHERE questId=${questId};` },
     UPDATE_Monsters :   function (monsterId, monsterName, monsterDesc, monsterTypeId, healthPool, attack, defense, speed) {
-                            return `UPDATE Monsters SET monsterName=${monsterName}, monsterDesc=${monsterDesc}, monsterTypeId=${monsterTypeId}, healthPool=${healthPool}, attack=${attack}, defense=${defense}, speed=${speed} WHERE monsterId=${monsterId};` },
+                            return `UPDATE Monsters SET monsterName='${monsterName}', monsterDesc='${monsterDesc}', monsterTypeId=${monsterTypeId}, healthPool=${healthPool}, attack=${attack}, defense=${defense}, speed=${speed} WHERE monsterId=${monsterId};` },
 
     UPDATE_Monsters_Abilities_Cooldown :    function (monsterId, abilityId, abilityCooldown) {
                                                 return `UPDATE Monsters_Abilities SET abilityCooldown=${abilityCooldown} WHERE (monsterId=${monsterId} and abilityId=${abilityId});` },
