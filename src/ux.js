84854436aa2d76f1f5825e6d022a2e16ec13ca14
Trenton Young <trentonyo@gmail.com>
2023-03-06 16:05:04 -0800

    ACTUALLY committing collapsing containers

--- src/ux.js
+++ src/ux.js
@@ -8,31 +8,58 @@ let column_loot = document.getElementById("lootItems_column")
 let add_ability = document.getElementById("addAbilityForm")
 let add_loot = document.getElementById("addLootItemForm")
 
-let nav_collapse_new = document.getElementById("collapse_new")
-let nav_collapse_view = document.getElementById("collapse_view")
+let collapse_buttons = document.getElementsByClassName('collapse-container')
 
-let toggleNewList = function (ev) {
-    let collapseTarget = document.querySelectorAll('.collapse-target.new')
-    for (let i = 0; i < collapseTarget.length; i++)
+let uncollapseChildren = function (event)
+{
+    let collapse_targets = event.target.closest(".collapse-container").getElementsByClassName("collapse-target")
+    console.log(collapse_targets)
+    for (let i = 0; i < collapse_targets.length; i++)
     {
-        collapseTarget[i].classList.toggle('hidden')
-        nav_collapse_new.classList.toggle('uncollapsed')
+        collapse_targets[i].classList.remove("hidden")
+    }
+}
+let collapseChildren = function (event)
+{
+    let collapse_targets = event.target.closest(".collapse-container").getElementsByClassName("collapse-target")
+    console.log(collapse_targets)
+    for (let i = 0; i < collapse_targets.length; i++)
+    {
+        collapse_targets[i].classList.add("hidden")
     }
 }
 
-nav_collapse_new.addEventListener('mouseenter', toggleNewList)
-nav_collapse_new.addEventListener('mouseleave', toggleNewList)
-
-let toggleViewList = function (ev) {
-    let collapseTarget = document.querySelectorAll('.collapse-target.view')
-    for (let i = 0; i < collapseTarget.length; i++) {
-        collapseTarget[i].classList.toggle('hidden')
-        nav_collapse_view.classList.toggle('uncollapsed')
-    }
+for (let i = 0; i < collapse_buttons.length; i++)
+{
+    collapse_buttons[i].addEventListener('mouseenter', uncollapseChildren)
+    collapse_buttons[i].addEventListener('mouseleave', collapseChildren)
 }
 
-nav_collapse_view.addEventListener('mouseenter', toggleViewList)
-nav_collapse_view.addEventListener('mouseleave', toggleViewList)
+// let nav_collapse_new = document.getElementById("collapse_new")
+// let nav_collapse_view = document.getElementById("collapse_view")
+//
+// let toggleNewList = function (ev) {
+//     let collapseTarget = document.querySelectorAll('.collapse-target.new')
+//     for (let i = 0; i < collapseTarget.length; i++)
+//     {
+//         collapseTarget[i].classList.toggle('hidden')
+//         nav_collapse_new.classList.toggle('uncollapsed')
+//     }
+// }
+//
+// nav_collapse_new.addEventListener('mouseenter', toggleNewList)
+// nav_collapse_new.addEventListener('mouseleave', toggleNewList)
+//
+// let toggleViewList = function (ev) {
+//     let collapseTarget = document.querySelectorAll('.collapse-target.view')
+//     for (let i = 0; i < collapseTarget.length; i++) {
+//         collapseTarget[i].classList.toggle('hidden')
+//         nav_collapse_view.classList.toggle('uncollapsed')
+//     }
+// }
+//
+// nav_collapse_view.addEventListener('mouseenter', toggleViewList)
+// nav_collapse_view.addEventListener('mouseleave', toggleViewList)
 
 
 for (let i = 0; i < input_fields.length; i++)
